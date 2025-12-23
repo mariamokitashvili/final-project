a4566099069cf3c83e7633485a00e3bbe6da3b47
mariamokitashvili <mariam.okitashvili13@gmail.com>
2025-12-23 15:02:05 +0400

    add testimonial

--- src/js/script.js
+++ src/js/script.js
@@ -59,3 +59,61 @@ filterBtns.forEach((btn) => {
     });
   });
 });
+
+//TESTIMONIAL SLIDER
+const testimonialSlides = [
+  {
+    img: "https://picsum.photos/200?random=20",
+    name: "გიორგი",
+    profession: "UI/UX designer",
+    text: "Smart Academy-ში სასწავლო პროგრამა კარგად არის დალაგებული — თითოეული თემა ლოგიკურად მიჰყვება ერთმანეთს და მასალას ადვილად ეწევი. ახალ სფეროში გადასასვლელად ნამდვილად გამოსადეგი სტარტია.",
+  },
+  {
+    img: "https://picsum.photos/200?random=25",
+    name: "ნანუკა",
+    profession: "ვიდეო კონტენტის შექმნა",
+    text: "ჯგუფური ფორმატი ძალიან კომფორტულია. ვინაიდან ჯგუფები პატარაა, ლექტორები ყველა სტუდენტს ინდივიდუალურად უდგებიან.",
+  },
+  {
+    img: "https://picsum.photos/200?random=45",
+    name: "ლაშა",
+    profession: "PR, EVENT BRANDING & MANAGEMENT",
+    text: "მომეწონა, რომ თეორიასთან ერთად პრაქტიკულ დავალებებსაც დიდი მნიშვნელობა აქვს. ბევრს მუშაობ მაგალითებზე და საბოლოოდ პრაქტიკასთან ერთად ცოდნა უკეთ გამახსოვრდება",
+  },
+  {
+    img: "https://picsum.photos/200?random=40",
+    name: "თამუნა",
+    profession: "FRONT-END DEVELOPMENT",
+    text: "აკადემიის გუნდი ყოველთვის სწრაფად პასუხობს ტექნიკურ თუ ორგანიზაციულ კითხვებზე. პროცესში მხარდაჭერა იგრძნობა და სასწავლო ეტაპი მარტივად მიმდინარეობს.",
+  },
+];
+
+let testimonialIndex = 0;
+
+const img = document.getElementById("slide-img");
+const nameEl = document.getElementById("slide-name");
+const professionEl = document.getElementById("slide-profession");
+const textEl = document.getElementById("slide-text");
+
+const dots = document.querySelectorAll(".dot");
+
+function showTestimonialSlide(i) {
+  testimonialIndex = i;
+  const s = testimonialSlides[i];
+
+  img.src = s.img;
+  nameEl.textContent = s.name;
+  professionEl.textContent = s.profession;
+  textEl.textContent = s.text;
+
+  dots.forEach((dot) => dot.classList.remove("active"));
+  dots[i].classList.add("active");
+}
+
+dots.forEach((dot) => {
+  dot.addEventListener("click", () => {
+    showTestimonialSlide(Number(dot.dataset.index));
+  });
+});
+
+showTestimonialSlide(0);
